281c6d636731540791065f9e6404cddcb18a5411
ZicoBacker <150025315+ZicoBacker@users.noreply.github.com>
2024-10-25 11:05:09 +0200

    dani ragebait merge conflcit push fix

--- Assets/Scripts/Objects/Entity.cs
+++ Assets/Scripts/Objects/Entity.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
-<<<<<<< HEAD
+
     public float Health = 100f;
-=======
     private void OnEnable()
     {
         // it will automatically set the database as the database object
@@ -15,9 +14,7 @@ public class Entity : MonoBehaviour
     }
     private ItemDatabaseObject database;
     public Dropchance[] dropTable;
-    public float Health = 100f;
     protected float maxHealth;
->>>>>>> 343e8fa243504e919dacceb1334f0c6e68c9a755
     public float Resistance;
     public float MagResistance;
     public int experienceReward = 25;
@@ -26,13 +23,14 @@ public class Entity : MonoBehaviour
 
     void Start()
     {
-<<<<<<< HEAD
+
         if (Health <= 0)
         {
             Destroy(gameObject);
-=======
-        
-        experienceManager = FindObjectOfType<ExperienceManager>();
+
+
+            experienceManager = FindObjectOfType<ExperienceManager>();
+        }
     }
 
     // virtual, in this case, is the keyword for override. It makes it possible to change the same method later on
@@ -50,41 +48,27 @@ public class Entity : MonoBehaviour
             Debug.Log("health is zero...");
             SpawnLoot();
             GrantExperience();
->>>>>>> 343e8fa243504e919dacceb1334f0c6e68c9a755
         }
     }
 
     public void TakePhysicalDmg(float Damage)
     {
-<<<<<<< HEAD
         float takenDamage = Damage - (0.75f * Resistance);
         Health -= takenDamage;
-=======
-        float takenDamge = Damage - (0.75f * Resistance);
-        Health -= takenDamge;
->>>>>>> 343e8fa243504e919dacceb1334f0c6e68c9a755
     }
-
     public void TakeMagicDmg(float Damage)
     {
-<<<<<<< HEAD
         float takenDamage = Damage - (0.75f * MagResistance);
         Health -= takenDamage;
-    }
-}
-=======
-        float takenDamge = Damage - (0.75f * MagResistance);
-        Health -= takenDamge;
     }
 
     private void GrantExperience()
     {
         if (experienceManager != null)
         {
-            experienceManager.AddExperience(experienceReward); 
+            experienceManager.AddExperience(experienceReward);
         }
     }
-
     public void SpawnLoot()
     {
         // don't do anything if it hasn't anything to drop.
@@ -99,19 +83,19 @@ public class Entity : MonoBehaviour
             float randomNumber = Random.Range(0f, 100f);
             Debug.Log(randomNumber);
             Debug.Log(dropTable[i].dropchance);
-
             // check if number is within the percentage
             if (randomNumber <= dropTable[i].dropchance)
             {
                 // spawn prefab
                 GameObject item = Instantiate(dropTable[i].DropItemPrefab, transform.position, Quaternion.identity);
-
                 // add the invenoryItem to the 3d object
                 item.GetComponent<GroundItem>().item = dropTable[i].item;
             }
         }
     }
+
 }
+
 [System.Serializable]
 public class Dropchance
 {
@@ -120,4 +104,3 @@ public class Dropchance
     public ItemObject item;
     public GameObject DropItemPrefab;
 }
->>>>>>> 343e8fa243504e919dacceb1334f0c6e68c9a755
